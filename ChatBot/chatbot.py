3d9fa5bf5e32413539580eda23a1443800297ac1
Babar Jawad Anjum <babarjawad010@gmail.com>
2026-08-30 19:24:52 +0500

    feat: persisting conversation context

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -5,13 +5,21 @@ load_dotenv()
 
 model = ChatOpenAI()
 
+chat_history = []
+
 while True:
     user_input = input("You: ")
+    
+    chat_history.append(user_input)
 
     if user_input == 'exit':
         break
 
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history)
+    
+    chat_history.append(result.content)
+
     print("Bot: ", result.content)
 
+print(chat_history)
     
