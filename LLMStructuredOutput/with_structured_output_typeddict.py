3cccf75524ffe92b8f347d58430dee22ff4224ea
Babar Jawad Anjum <babarjawad010@gmail.com>
2026-09-01 01:17:22 +0500

    feat: use Annotated while defining schema - typeddict

--- LLMStructuredOutput/with_structured_output_typeddict.py
+++ LLMStructuredOutput/with_structured_output_typeddict.py
@@ -1,21 +1,33 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict, Annotated, Optional
 
 load_dotenv()
 
-model = ChatOpenAI()
+model = ChatOpenAI(model="gpt-4o-mini")
 
 # Schema
 class Review(TypedDict):
-    summary: str
-    sentiment: str
+    key_themes: Annotated[list[str], "Write down all the key themes in a list discussed in the review"]
+    summary: Annotated[str, "A brief summary of the review"]
+    sentiment: Annotated[str, "Return sentiment of the review either positive, negative or neutral"]
+    pros: Annotated[Optional[list[str]], "Write down all the pros in a list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons in a list"]
 
 structured_model = model.with_structured_output(Review)
 
 # Instead of invoking model, we will invoke structured_model
-result = structured_model.invoke("""The hardware is great, but the software feels bloated. There are 
-    too many pre-installed apps that I can't remove. Also, the UI looks outdated compared to other brands. 
-    Hoping for a software update to fix this.""")
+result = structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+""")
 
 print(result)
