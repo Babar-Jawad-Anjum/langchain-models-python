ab5c2329afc075a4307d3e4a4b42bf05014b9d76
Babar Jawad Anjum <babarjawad010@gmail.com>
2026-09-01 16:01:38 +0500

    feat: json output parser

---
+++ OutputParsers/jsonOutputParser.py
@@ -0,0 +1,35 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct", # This model didn't support structured output by default
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template='Give me the name, age and city of a fictional person \n {format_instruction}',
+    input_variables=[],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+final_result = chain.invoke({})
+
+# We can replace below code (line 29 - 33) with above (line 23 - 25)
+
+# prompt = template.format()
+
+# result = model.invoke(prompt)
+
+# final_result = parser.parse(result.content)
+
+print(final_result)
