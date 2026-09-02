464aaf155f1589343239e2b05f65206cb23aaa64
Babar Jawad Anjum <babarjawad010@gmail.com>
2026-09-02 15:41:38 +0500

    feat: sequential chains

---
+++ Chains/sequential_chain.py
@@ -0,0 +1,27 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template="Generate 5 interesting facts about {topic}",
+    input_variables=["topic"]
+)
+
+prompt2 = PromptTemplate(
+    template="Generate a 5 pointer summary on following text \n {text}",
+    input_variables=["text"]
+)
+
+model = ChatOpenAI(model="gpt-4o-mini")
+
+parser = StrOutputParser()
+
+
+chain = prompt1 | model | parser | prompt2 | model | parser
+
+result = chain.invoke({'topic': 'Employment in USA'})
+
+print(result)
